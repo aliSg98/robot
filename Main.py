165f1cc2170220a6320efc73d26d3490f26caf73
aliSg98 <ali_sg@outlook.com>
2024-07-23 12:47:29 +0200

    robot1.2

---
+++ Main.py
@@ -0,0 +1,11 @@
+from src.ConnexionPostgress import ConnexionPostgress
+
+def main():
+    database = ConnexionPostgress()
+    database.createTable()
+    database.insert()    
+    database.closeConexion()
+
+
+if(__name__ == '__main__'):
+    main()
---
+++ src/ConnexionPostgress.py
@@ -0,0 +1,51 @@
+import psycopg2
+
+class ConnexionPostgress:
+    """Constructor que inicia conexion a base de datos"""
+    def __init__(self):
+        try:
+            self._conexion = psycopg2.connect(
+                host = 'localhost',
+                port = '5432',
+                user = 'postgres',
+                password = 'admin',
+                database = 'mydatabase'
+                
+            )
+            print("Conexion correcta")                    
+
+        except Exception as exception:
+            print(exception)
+    
+    """Crear tabla"""
+    def createTable(self):
+        self._cursor = self._conexion.cursor()        
+        sql_create = """CREATE TABLE IF NOT EXISTS robot(
+            name_robot TEXT PRIMARY KEY,
+            status_creation TEXT,
+            status_pdf TEXT,
+            path_pdf TEXT,
+            status_final TEXT
+        )"""
+        self._cursor.execute(sql_create)
+        self._conexion.commit()
+
+    """Hacer insert en la tabla"""
+    def insert (self):
+        sql = 'INSERT INTO robot(name_robot,status_creation,status_pdf,path_pdf,status_final) values (%s,%s,%s,%s,%s)'
+        nombre = input("Ingrese nombre robot: ")
+        status = input("Ingrese estado de creacion: ")
+        status_pdf = input("Ingrese estado pdf: ")
+        path = input('Ingrese path pdf: ')
+        final_status = input("Ingrese estado final: ")
+
+        datos = (nombre,status,status_pdf,path,final_status)
+        self._cursor.execute(sql,datos)
+        #guardar datos
+        self._conexion.commit()
+
+    """Cerrar conexion"""
+    def closeConexion(self):
+        self._cursor.close()
+        self._conexion.close()
+
