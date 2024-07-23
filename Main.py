9d3a90a81c1379d16d5505d6f899f4e08147c680
aliSg98 <ali_sg@outlook.com>
2024-07-23 14:26:40 +0200

    robot1.3.3

--- Main.py
+++ Main.py
@@ -1,9 +1,20 @@
 from src.ConnexionPostgress import ConnexionPostgress
 
 def main():
+    """Conectarse a la base de datos"""
     database = ConnexionPostgress()
-    database.createTable()
-    #database.insert()    
+
+    """Elegir si quieres crear la tabla o hacer un insert con los datos que quieras"""
+    num = int(input("Ingrese 1 para Crear tabla, 2 para hacer INSERT"))    
+    # match case
+    match num:
+        case num if num == 1:
+            database.createTable()
+        case num if num == 2:
+            database.insert() 
+        case _:
+            print("Error") 
+    """Cerrar base de datos"""
     database.closeConexion()
 
 
--- robot/tasks.py
+++ robot/tasks.py
@@ -1,41 +1,27 @@
 from robocorp.tasks import task
+from RPA.Excel.Files import Files
 from robocorp import browser
 
 from RPA.HTTP import HTTP
 
 @task
-def robot_spare_bin_python():
+def robot_python():
     """Insert the sales data for the week and export it as a PDF"""
     browser.configure(
         slowmo=100,
     )
-    open_the_intranet_website()
-    log_in()
-    download_excel_file()
-    fill_and_submit_sales_form()
-
-def open_the_intranet_website():
-    """Navigates to the given URL"""
-    browser.goto("https://robotsparebinindustries.com/")
-
-def log_in():
-    """Fills in the login form and clicks the 'Log in' button"""
-    page = browser.page()
-    page.fill("#username", "maria")
-    page.fill("#password", "thoushallnotpass")
-    page.click("button:text('Log in')")
-
-def fill_and_submit_sales_form():
-    """Fills in the sales data and click the 'Submit' button"""
-    page = browser.page()
-
-    page.fill("#firstname", "John")
-    page.fill("#lastname", "Smith")
-    page.fill("#salesresult", "123")
-    page.select_option("#salestarget", "10000")
-    page.click("text=Submit")
-
-def download_excel_file():
-    """Downloads excel file from the given URL"""
-    http = HTTP()
-    http.download(url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
+    createExcel()
+    
+"""Creacion excel"""
+def createExcel():
+    lib = Files()
+    lib.create_workbook(path=r"C:\Users\nasudre\Desktop\Robot\robot\output\robot.xlsx", fmt="xlsx")
+    Worksheet_Data = {
+    "Name_robot": ["ABC8273", "ABC82763", "ABC82673"],
+    "Status_creation": ["DONE","DONE","WIP"],
+    "Status_pdf": ["DONE","DONE","WIP"],
+    "Path_pdf": ["/users/Ali","/users/Jorge","/users/Alex"],
+    "Status_final": ["DONE","DONE","WIP"]
+    }    
+    lib.create_worksheet(name="robot.xlsx",content=Worksheet_Data,header=True)
+    lib.save_workbook()
