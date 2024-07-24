14d666a4371ef2797b481fda4d242b3fb5738fd9
aliSg98 <ali_sg@outlook.com>
2024-07-24 15:01:48 +0200

    robot1.4.2

--- src/ReportExcel.py
+++ src/ReportExcel.py
@@ -1,5 +1,7 @@
 import pandas
 import os
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 
 class ReportExcel:
     """creo fichero excel con sus columnas y algunos datos"""
@@ -14,9 +16,35 @@ class ReportExcel:
             "Status_final": ["DONE","DONE","WIP"]
         })
         os.makedirs(os.path.dirname(path_xlsx),exist_ok=True)
-        self._columnas.to_excel(path_xlsx, sheet_name='Robot1')
-        #self._excel_writer._save()
+        self._columnas.to_excel(path_xlsx, index = False, sheet_name='Robot1')
+        
+        #Cargar el fichero excel con openpyxl
+        open_wb = load_workbook(path_xlsx)
+        res = open_wb.active
+
+        #Definir color azul
+        color = PatternFill(start_color="ADD8E6",end_color="ADD8E6",fill_type="solid")
+
+        #solo cojo los encabezados de la primera fila
+        for col in res[1]:
+            col.fill = color
+        #guardar excel con el nuevo color
+        open_wb.save(path_xlsx)
+
+        """Cambiar color segun sea wip -> amarillo, done --> verde, fail --> rojo"""
+        color_wip = PatternFill(start_color="FFFF00",end_color="FFFF00",fill_type="solid")
+        color_done = PatternFill(start_color="00FF00",end_color="00FF00",fill_type="solid")
+        color_fail = PatternFill(start_color="FF0000",end_color="FF0000",fill_type="solid")
+
+        #columans que me interesa cambiar el color
+        columnas_cambiar = [2,3,5]
+        for columna in res.iter_rows(min_row=2,max_row=res.max_row):
+            for col in columnas_cambiar:
+                celda=columna[col-1]
+                cell_value = str(celda.value).strip().lower()
+                if cell_value in ['wip']: celda.fill = color_wip
+                elif cell_value in ['done']: celda.fill = color_done
+                elif cell_value in ['fail']: celda.fill = color_fail
+        open_wb.save(path_xlsx)
 
         print('Excel generado')
-
-   
