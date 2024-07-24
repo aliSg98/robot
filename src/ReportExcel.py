45b012010b6adba6236865ef606720cb27d82011
aliSg98 <ali_sg@outlook.com>
2024-07-24 15:06:44 +0200

    robot1.4.3

--- src/ReportExcel.py
+++ src/ReportExcel.py
@@ -4,22 +4,22 @@ from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
 class ReportExcel:
-    """creo fichero excel con sus columnas y algunos datos"""
+    """creo fichero excel con sus columnas y algunos datos, y añado colores"""
     def __init__(self):
-        path_xlsx = r"C:\Users\nasudre\Desktop\Robot\LOG\Robot.xlsx"
+        self._path_xlsx = r"C:\Users\nasudre\Desktop\Robot\LOG\Robot.xlsx"
 
         self._columnas = pandas.DataFrame({
-            "Name_robot": ["ABC8273", "ABC82763", "ABC82673"],
-            "Status_creation": ["DONE","DONE","WIP"],
-            "Status_pdf": ["DONE","DONE","WIP"],
-            "Path_pdf": ["/users/Ali","/users/Jorge","/users/Alex"],
-            "Status_final": ["DONE","DONE","WIP"]
+            "Name_robot": ["ABC8273", "ABC82763", "ABC82673","AHJD77"],
+            "Status_creation": ["DONE","DONE","WIP","FAIL"],
+            "Status_pdf": ["DONE","DONE","WIP","FAIL"],
+            "Path_pdf": ["/users/Ali","/users/Jorge","/users/Alex","users/Pedro"],
+            "Status_final": ["DONE","DONE","WIP","Fail"]
         })
-        os.makedirs(os.path.dirname(path_xlsx),exist_ok=True)
-        self._columnas.to_excel(path_xlsx, index = False, sheet_name='Robot1')
+        os.makedirs(os.path.dirname(self._path_xlsx),exist_ok=True)
+        self._columnas.to_excel(self._path_xlsx, index = False, sheet_name='Robot1')
         
         #Cargar el fichero excel con openpyxl
-        open_wb = load_workbook(path_xlsx)
+        open_wb = load_workbook(self._path_xlsx)
         res = open_wb.active
 
         #Definir color azul
@@ -29,7 +29,7 @@ class ReportExcel:
         for col in res[1]:
             col.fill = color
         #guardar excel con el nuevo color
-        open_wb.save(path_xlsx)
+        open_wb.save(self._path_xlsx)
 
         """Cambiar color segun sea wip -> amarillo, done --> verde, fail --> rojo"""
         color_wip = PatternFill(start_color="FFFF00",end_color="FFFF00",fill_type="solid")
@@ -40,11 +40,13 @@ class ReportExcel:
         columnas_cambiar = [2,3,5]
         for columna in res.iter_rows(min_row=2,max_row=res.max_row):
             for col in columnas_cambiar:
+                #openpyxl usa indices base 1
                 celda=columna[col-1]
+                #paso a minusculas para que me coja independientemente sea mayus o minus
                 cell_value = str(celda.value).strip().lower()
                 if cell_value in ['wip']: celda.fill = color_wip
                 elif cell_value in ['done']: celda.fill = color_done
                 elif cell_value in ['fail']: celda.fill = color_fail
-        open_wb.save(path_xlsx)
+        open_wb.save(self._path_xlsx)
 
         print('Excel generado')
